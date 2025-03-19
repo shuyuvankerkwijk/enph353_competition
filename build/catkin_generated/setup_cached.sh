95914e49d73d717cbd40e2051576bc40f2f644a8
benedikthoward <benedikthoward@gmail.com>
2025-03-19 16:17:50 -0700

    didn't want to re-do all those file paths etc this is probs better

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,10 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/fizzer/ros_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/fizzer/ros_ws/devel/lib:/opt/ros/noetic/lib:/usr/local/cuda-11.2/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/cuda-11.2/bin:/home/fizzer/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/fizzer/ros_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/fizzer/ros_ws/build'
+export PYTHONPATH="/home/fizzer/ros_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/fizzer/ros_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/fizzer/ros_ws/src:$ROS_PACKAGE_PATH"
